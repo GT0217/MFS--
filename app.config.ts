ba98375084c7c0638e87bb5fe1426bdfd9fe6cba
GT0217 <captinkkt8@gmail.com>
2026-06-06 20:44:07 +0900

    Add files via upload

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    preset: "vercel",
+  },
   vite: {
     plugins: [
       viteTsConfigPaths({
